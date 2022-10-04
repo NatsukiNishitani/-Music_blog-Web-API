9cc3b30863f899830d7509303cc69857fdd8f67c
NatsukiNishitani <tanishiyaaa0717@gmail.com>
2022-10-04 14:50:14 +0900

    テーブル作成完了

---
+++ database/migrations/2022_10_02_054542_create_musics_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('musics', function (Blueprint $table) {
+            $table->id();
+            $table->string('song_title', 50);
+            $table->string('singer', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('musics');
+    }
+};
