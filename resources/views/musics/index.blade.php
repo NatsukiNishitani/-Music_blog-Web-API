345e52e1101bb1294e83921bf427b6458dccc0af
NatsukiNishitani <tanishiyaaa0717@gmail.com>
2022-10-04 17:05:52 +0900

    breeze機能＋曲投稿画面途中

---
+++ resources/views/musics/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Music</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <div class='musics'>
+            <div class='music'>
+                <h1>曲投稿（music upload）</h1>
+                <h2 class='Song title'>曲名（song title）</h2>
+                <h2 class='singer'>歌手(singer)</h2>
+                <p class='hashtags'>#ハッシュタグ(tag)</p>                                                                                                                                                                                              
+            </div>
+        </div>
+    </body>
+</html>
